0130a7afe044e758c2f8eb9a30bd854a4e154f54
WeChatScanHelper <167905791+WeChatScanHelper@users.noreply.github.com>
2026-01-12 22:17:31 +0800

    Update hand.py

--- hand.py
+++ hand.py
@@ -2,14 +2,13 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 import cv2
 import mediapipe as mp
-# Force import of the hands solution
-from mediapipe.python.solutions import hands as mp_hands
-from mediapipe.python.solutions import drawing_utils as mp_draw
 
-# Then initialize your hands object using the imported mp_hands
-hands = mp_hands.Hands(
-    static_image_mode=False, 
-    max_num_hands=1, 
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5
-)
+# Use this robust initialization
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+# For simple legacy drawing (which still works in latest versions):
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
